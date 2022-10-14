78f8d63c97a898a978e4a50303ceb1ad9914a682
casperYN <amx004qubeleymk3@gmail.com>
2022-10-14 14:55:49 +0900

    local初回

---
+++ src/jp/co/kicnet/nakano/test01/HelloWorld.java
@@ -0,0 +1,17 @@
+package jp.co.kicnet.nakano.test01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	
+	
+		System.out.println("NakanoTest!!!") ;
+	
+	
+		//はじめはこの行しか書いてない　2022.10.14　Nakano
+	
+	}
+
+}
